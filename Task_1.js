6cadbd5ecf1cc0c006b42ff7d7841c72036eabcb
ivan-ver <ivan_ver89@mail.ru>
2019-05-30 06:01:08 +0400

    Исправление 1

--- Task_1.js
+++ Task_1.js
@@ -1,7 +1,7 @@
 sum_button.onclick = function () {
     let a = document.getElementById("number_1").value;
     let b = document.getElementById("number_2").value;
-    let maxLen = a.length > b.length ? a.length:a.length;
+    let maxLen = a.length > b.length ? a.length:b.length;
     let result = ((Number(a)*(10**maxLen))+(Number(b)*(10**maxLen)))/(10**maxLen);
     alert(result);
 };
